ec87949c35cb236f592da575aee6b6fa7126007a
andrew-qian <andrewyukaiqian@gmail.com>
2022-10-20 10:04:44 -0500

    Finished PigLatin.java

--- M359_GettingStarted/src/Unit4/PigLatinLab/PigLatin.java
+++ M359_GettingStarted/src/Unit4/PigLatinLab/PigLatin.java
@@ -1,5 +1,7 @@
 package Unit4.PigLatinLab;
 
+import java.util.Locale;
+
 public class PigLatin {
     // create toPigLatin method; make it so that it uses translateWordToPigLatin
     // every time there is a space
@@ -11,13 +13,23 @@ public class PigLatin {
     //if vowel  add yay to the end
     // str ans = str + yay;
 
-    public static void toPigLatin(String str){
+    public static String toPigLatin(String str){
+        String ans = "";
         int index = 0;
-        for (int i )
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == ' '){
+                ans += translateWordToPigLatin(str.substring(index, i)) + " ";
+                index = i+1;
+            }
+        }
+        ans += translateWordToPigLatin(str.substring(index));
+        ans = ans.substring(0,1).toUpperCase() + ans.substring(1);
+        return ans;
     }
 
     public static String translateWordToPigLatin(String str){
         String ans = "";
+        str = str.toLowerCase();
         if (checkFirstVowel(str)){
             ans = str + "yay";
             return ans;
