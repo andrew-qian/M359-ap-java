e564251ac3f4be349a4cb43de9e48639cb80661a
andrew-qian <andrewyukaiqian@gmail.com>
2022-11-11 10:09:11 -0600

    PartnerLab

--- M359_GettingStarted/src/Unit5/PartnerLab/Tester.java
+++ M359_GettingStarted/src/Unit5/PartnerLab/Tester.java
@@ -6,7 +6,7 @@ import static Unit5.PartnerLab.SimulateSeason.simulateSeason;
 public class Tester {
     public static void main(String[] args) {
         Team bears = new Team(1, 1, "Bears");
-        for (int i = 0; i < 200; i++){
+        for (int i = 0; i < 5; i++){
             System.out.println(simulateSeason(bears));
         }
         System.out.println(franchiseLog());
