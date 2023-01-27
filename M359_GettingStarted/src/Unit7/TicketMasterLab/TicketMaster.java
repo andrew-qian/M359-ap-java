1e4fda7d1242dfa111412a975fd6e047be308613
andrew-qian <andrewyukaiqian@gmail.com>
2023-01-27 10:09:11 -0600

    push

--- M359_GettingStarted/src/Unit7/TicketMasterLab/TicketMaster.java
+++ M359_GettingStarted/src/Unit7/TicketMasterLab/TicketMaster.java
@@ -1,17 +1,15 @@
 package Unit7.TicketMasterLab;
 
+import javax.crypto.AEADBadTagException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class TicketMaster {
-    private static final int SORT_AZ = 1;
-    private static final int SORT_ZA = 2;
-    private static final int SORT_LH = 3;
-    private static final int SORT_HL = 4;
-    private static final int SEARCH_CITY = 5;
-    private static final int QUIT = 6;
+
     private static ArrayList<Show> allShows;
 
     public TicketMaster(ArrayList<Show> allShows) {
@@ -27,56 +25,12 @@ public class TicketMaster {
         return str;
     }
 
-    public static void choiceMenu(Scanner s){
-        System.out.println("Input: ");
-        int userInput = -1;
-        System.out.println("1: Sort A-Z");
-        System.out.println("2: Sort Z-A");
-        System.out.println("3: Sort low-high (ticket price)");
-        System.out.println("4: Sort high-low (ticket price)");
-        System.out.println("5: Search by city");
-        System.out.println("6: Quit");
 
-        try{
-            userInput = s.nextInt();
-            s.nextLine();
-        } catch (Exception e){
-            System.out.println("TYPE ERROR");
-            s.nextLine();
-            choiceMenu(s);
-        }
-        while (userInput > 6 || userInput < 1){
-            System.out.println("Out of range.");
-            choiceMenu(s);
-        }
-        if (userInput == SORT_AZ){
-            optionOne(s);
-        }
-        else if (userInput == SORT_ZA){
-            optionTwo(s);
-        }
-        else if (userInput == SORT_LH){
-            System.out.println("Option three.");
-            optionThree(s);
-        }
-        else if (userInput == SORT_HL){
-            System.out.println("Option four.");
-            optionFour(s);
-        }
-        else if (userInput == SEARCH_CITY){
-            optionFive(s);
-
-        }
-        else if (userInput == QUIT){
-            System.out.println("Thank you for using TicketMaster!");
-            System.exit(0);
-        }
-    }
     public static void optionOne(Scanner s){
         sortByPerformer(true);
         TicketMaster AZ = new TicketMaster(allShows);
         System.out.println(AZ);
-        choiceMenu(s);
+        TicketMasterDriver.choiceMenu(s);
     }
 
 
@@ -84,7 +38,7 @@ public class TicketMaster {
         sortByPerformer(false);
         TicketMaster ZA = new TicketMaster(allShows);
         System.out.println(ZA);
-        choiceMenu(s);
+        TicketMasterDriver.choiceMenu(s);
     }
 
     public static void sortByPerformer(Boolean AZ){
@@ -113,10 +67,10 @@ public class TicketMaster {
         }
     }
     public static void optionThree(Scanner s){
-        choiceMenu(s);
+        TicketMasterDriver.choiceMenu(s);
     }
     public static void optionFour(Scanner s){
-        choiceMenu(s);
+        TicketMasterDriver.choiceMenu(s);
     }
 
     public static void optionFive(Scanner s){
@@ -129,10 +83,11 @@ public class TicketMaster {
             optionFive(s);
         }
         searchCity(userInput);
-        choiceMenu(s);
+        TicketMasterDriver.choiceMenu(s);
     }
 
     public static void searchCity(String userInput){
+        ArrayList<Show> backup = allShows;
         ArrayList<Show> showInCity = new ArrayList<>();
         for (Show show: allShows){
             if (show.getCity().equalsIgnoreCase(userInput)){
@@ -145,6 +100,7 @@ public class TicketMaster {
         else{
             TicketMaster citySort = new TicketMaster(showInCity);
             System.out.println(citySort);
+            TicketMaster backupObj = new TicketMaster(backup);
         }
     }
 
--- M359_GettingStarted/src/Unit7/TicketMasterLab/TicketMasterDriver.java
+++ M359_GettingStarted/src/Unit7/TicketMasterLab/TicketMasterDriver.java
@@ -2,10 +2,64 @@ package Unit7.TicketMasterLab;
 
 import java.util.Scanner;
 
+
 public class TicketMasterDriver {
+    private static final int SORT_AZ = 1;
+    private static final int SORT_ZA = 2;
+    private static final int SORT_LH = 3;
+    private static final int SORT_HL = 4;
+    private static final int SEARCH_CITY = 5;
+    private static final int QUIT = 6;
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         TicketMaster allShows = new TicketMaster(TicketMaster.readFile("showData.txt"));
-        allShows.choiceMenu(s);
+        choiceMenu(s);
     }
+
+    public static void choiceMenu(Scanner s){
+        System.out.println("Input: ");
+        int userInput = -1;
+        System.out.println("1: Sort A-Z");
+        System.out.println("2: Sort Z-A");
+        System.out.println("3: Sort low-high (ticket price)");
+        System.out.println("4: Sort high-low (ticket price)");
+        System.out.println("5: Search by city");
+        System.out.println("6: Quit");
+
+        try{
+            userInput = s.nextInt();
+            s.nextLine();
+        } catch (Exception e){
+            System.out.println("TYPE ERROR");
+            s.nextLine();
+            choiceMenu(s);
+        }
+        while (userInput > 6 || userInput < 1){
+            System.out.println("Out of range.");
+            choiceMenu(s);
+        }
+        if (userInput == SORT_AZ){
+            TicketMaster.optionOne(s);
+        }
+        else if (userInput == SORT_ZA){
+            TicketMaster.optionTwo(s);
+        }
+        else if (userInput == SORT_LH){
+            System.out.println("Option three.");
+            TicketMaster.optionThree(s);
+        }
+        else if (userInput == SORT_HL){
+            System.out.println("Option four.");
+            TicketMaster.optionFour(s);
+        }
+        else if (userInput == SEARCH_CITY){
+            TicketMaster.optionFive(s);
+
+        }
+        else if (userInput == QUIT){
+            System.out.println("Thank you for using TicketMaster!");
+            System.exit(0);
+        }
+    }
+
 }
