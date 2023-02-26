f512bd558cc3ab8672812cba1ac99b0ad8169843
andrew-qian <andrewyukaiqian@gmail.com>
2023-02-26 10:19:53 -0600

    Sch WS1

--- M359_GettingStarted/src/Unit9/TeamAthlete.java
+++ M359_GettingStarted/src/Unit9/TeamAthlete.java
@@ -1,4 +1,28 @@
 package Unit9;
 
-public class TeamAthlete {
+public class TeamAthlete extends Athlete{
+    private String teamName;
+    private String sport;
+
+    public TeamAthlete(String teamName, String sport, String name, int age){
+        super(name, age);
+        this.teamName = teamName;
+        this.sport = sport;
+    }
+
+    public void setTeamName(String teamName){
+        this.teamName = teamName;
+    }
+
+    public void setSport(String sport){
+        this.sport = sport;
+    }
+
+    public String getTeamName(){
+        return teamName;
+    }
+
+    public String getSport(){
+        return sport;
+    }
 }
